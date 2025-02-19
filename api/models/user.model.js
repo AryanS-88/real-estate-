70be0e5e134bbfabb7f6a6b0c7612a0eceffae8a
AryanS-88 <ssingharyan70@gmail.com>
2025-02-19 16:06:27 +0530

    Git Store

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,5 +1,5 @@
-import { serialize } from 'mongodb'
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+
 const userSchema = new mongoose.Schema(
   {
     username: {
@@ -16,8 +16,14 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    avatar:{
+      type: String,
+      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+    },
   },
   { timestamps: true }
-)
-const User = mongoose.model('User', userSchema)
-export default User
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
